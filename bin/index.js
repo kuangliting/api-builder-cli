0365a87065a4adcff7e1515245428aba70e4057c
kuangliting <kuangliting_vendor@domain.sensetime.com>
2020-06-03 18:16:51 +0800

    add create type file

--- bin/index.js
+++ bin/index.js
@@ -3,23 +3,33 @@
 const {
   program
 } = require('commander');
-
+const version = require("../package.json").version;
 program
-  .version('0.1.0')
+  .version(version)
   .usage('<command> [options]')
-  .option('-host, --host <host>',' swagger文件服务器地址')
+  .option('-host, --host <host>', ' swagger文件服务器地址')
   .option('-ap, --api-path <apiPath>', 'api生成路径')
   .option('-sp, --s-path <swaggerPath>', 'swagger.json保存路径')
   .option('-tp, --t-path <typesPath>', 'ts 接口生成路径')
-  .action(function ({host,apiPath,sPath,tPath}) {
-    let args = {host,apiPath,sPath,tPath};
+  .action(function ({
+    host,
+    apiPath,
+    sPath,
+    tPath
+  }) {
+    let args = {
+      host,
+      apiPath,
+      sPath,
+      tPath
+    };
     require("../lib/build")(args)
   })
 
-  program
+program
   .command('init')
   .description('run setup commands for all envs')
-  .action(function(){
+  .action(function () {
     require("../lib/init")()
   });
 
